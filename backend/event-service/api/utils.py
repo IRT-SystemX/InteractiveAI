e42444d821683e60f2f30f18f2221d9745670921
Ilyes KAANICH <ilyeskaanich@gmail.com>
2022-12-05 15:45:44 +0100

    [35] Fix 500 error on event update

--- backend/event-service/api/utils.py
+++ backend/event-service/api/utils.py
@@ -8,7 +8,7 @@ def get_event_id(input_line, use_case):
         events_list = EventModel.query.filter_by(use_case=use_case).all()
 
         for event in events_list:
-            if event.data["line"] == input_line:
+            if event.data.get("line") == input_line:
                 event_id = event.id_event
                 logging.debug(f"Found event: {event_id} containing line: {input_line}")
                 return str(event_id)
