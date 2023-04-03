8eaff46ddd0aa8aed28db47a08051a3db4fba2f6
Ilyes KAANICH <ilyeskaanich@gmail.com>
2023-04-03 15:09:01 +0200

    Change GET recommendation to POST

--- backend/recommendation-service/api/schemas.py
+++ backend/recommendation-service/api/schemas.py
@@ -5,10 +5,3 @@ from apiflask.fields import DateTime, String, Dict
 class RecommendationAsk(Schema):
     context = Dict()
     event = Dict()
-
-
-class BaseRecommendation(Schema):
-    id_recommendation = String()
-    timestamp_start = DateTime(format="iso")
-    timestamp_end = DateTime(format="iso")
-    data = Dict()
