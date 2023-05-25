389c0557eb31b562962be4981e2527a4e04b9472
Ilyes KAANICH <ilyeskaanich@gmail.com>
2023-05-25 10:59:13 +0200

    Remove SQLALCHEMY_TRACK_MODIFICATIONS from event & context services

--- backend/context-service/config.py
+++ backend/context-service/config.py
@@ -7,11 +7,9 @@ class Config:
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     TESTING = False
 
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     TESTING = True
