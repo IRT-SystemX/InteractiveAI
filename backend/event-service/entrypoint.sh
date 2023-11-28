0df6f620304ec1f1fa612cd0f1fb34d66faaaef0
Ilyes KAANICH <ilyeskaanich@gmail.com>
2023-11-28 19:55:46 +0100

    [Event] Add migration commands to entrypoint.sh

--- backend/event-service/entrypoint.sh
+++ backend/event-service/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # export CARDS_PUBLICATION_SERVICE="http://cards-publication:8080"
 # export GATEWAY_SERVICE="http://web-ui:80"
-python -m flask run --host=0.0.0.0 --reload
+flask db init
+flask db migrate
+flask db upgrade
+flask run --host=0.0.0.0 --reload
