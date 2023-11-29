ae4a9e94ee252bc1141bed7948c9b727583dc663
d00r5 <174354444+d00r5@users.noreply.github.com>
2023-11-29 18:13:01 +0100

    feat: add draft for components

---
+++ frontend/main/src/shared/components/ProcedureView/Step/component.js
@@ -0,0 +1,21 @@
+class ProcedureStepComponent extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    fetch('./shared/components/ProcedureView/Step/template.html')
+      .then((response) => response.text())
+      .then((html) => {
+        this.shadowRoot.innerHTML = html;
+        this.addEventListener('mouseenter', this.showLink.bind(this));
+        this.addEventListener('mouseleave', hideLink);
+        this.addEventListener('click', () => this.setAttribute('style', 'filter:grayscale(1)'));
+      })
+      .catch((error) => console.error('Error fetching HTML:', error));
+  }
+
+  showLink() {
+    showLink(+this.getAttribute('source'), +this.getAttribute('target'));
+  }
+}
+
+customElements.define('procedure-step', ProcedureStepComponent);
