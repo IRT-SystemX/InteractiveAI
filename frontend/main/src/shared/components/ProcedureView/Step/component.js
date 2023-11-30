a42ce85a98e9e6995f1524bf298ce9fc28d8a702
d00r5 <174354444+d00r5@users.noreply.github.com>
2023-11-30 12:45:13 +0100

    feat: step logic

--- frontend/main/src/shared/components/ProcedureView/Step/component.js
+++ frontend/main/src/shared/components/ProcedureView/Step/component.js
@@ -6,9 +6,47 @@ class ProcedureStepComponent extends HTMLElement {
       .then((response) => response.text())
       .then((html) => {
         this.shadowRoot.innerHTML = html;
-        this.addEventListener('click', () => this.setAttribute('state', 'done'));
+        this.addEventListener('click', () => this.nextStep());
+        this.shadowRoot.getElementById('procedure-step-landing').addEventListener('click', () => this.getRecommandationDA('test'));
       })
       .catch((error) => console.error('Error fetching HTML:', error));
+  }
+
+  nextStep() {
+    if (this.getAttribute('state') === 'doing') {
+      this.setAttribute('state', 'done');
+      document
+        .getElementsByTagName('procedure-component')[0]
+        .shadowRoot.querySelector(`procedure-step[step="${+this.getAttribute('step') + 1}"]`)
+        .setAttribute('state', 'doing');
+    }
+  }
+
+  getRecommandationDA(title) {
+    document.getElementsByTagName('procedure-component')[0].hidden = true;
+    var recoResponse;
+    var data = JSON.stringify({
+      event: {
+        event_type: title,
+      },
+    });
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.addEventListener('readystatechange', function () {
+      if (this.readyState === 4) {
+        document.getElementById('da_block_request').innerHTML = '';
+        document.getElementById('da_block_request').innerHTML += 'Procedure <hr>';
+        recoResponse = JSON.parse(this.responseText);
+        Object.keys(recoResponse).forEach(function (k) {
+          document.getElementById('da_block_request').hidden = false;
+          document.getElementById('da_block_request').innerHTML += recoResponse[k].title + '<hr>';
+        });
+      }
+    });
+    xhr.open('POST', host + '/cab_recommendation/api/v1/recommendation');
+    xhr.setRequestHeader('Authorization', 'Bearer ' + window.localStorage.token);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(data);
   }
 }
 
