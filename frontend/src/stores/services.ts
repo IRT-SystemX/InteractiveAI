95d8e56a274e7a7d03edbf715e6c2d5d81f27fbc
d00r5 <174354444+d00r5@users.noreply.github.com>
2024-01-25 13:28:18 +0100

    fix: if context request fails, clear interval

--- frontend/src/stores/services.ts
+++ frontend/src/stores/services.ts
@@ -1,11 +1,14 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
 
 import * as servicesApi from '@/api/services'
+import eventBus from '@/plugins/eventBus'
 import type { Context, Entity } from '@/types/entities'
 import type { Recommendations } from '@/types/services'
 
 export const useServicesStore = defineStore('services', () => {
+  const { t } = useI18n()
   const context = ref<Context | undefined>()
   const recommendations = ref<Recommendations[]>([])
 
@@ -14,13 +17,31 @@ export const useServicesStore = defineStore('services', () => {
     callback: (context: T) => void,
     delay = 5000
   ) {
+    const modalID = crypto.randomUUID()
+    let contextPID = 0
+    eventBus.on('modal:close', (data) => {
+      console.log('salut')
+      if (data.id === modalID && data.res === 'ok') {
+        handler()
+        contextPID = window.setInterval(handler, delay)
+      }
+    })
     const handler = async () => {
-      const { data } = await servicesApi.getContext()
-      context.value = data.find((el) => el.use_case === entity)?.data
-      if (context.value) callback(context.value as T)
+      try {
+        const { data } = await servicesApi.getContext()
+        context.value = data.find((el) => el.use_case === entity)?.data
+        if (context.value) callback(context.value as T)
+      } catch (err) {
+        clearInterval(contextPID)
+        eventBus.emit('modal:open', {
+          data: t('modal.error.CONTEXT_FAILED'),
+          type: 'choice',
+          id: modalID
+        })
+      }
     }
     handler()
-    return window.setInterval(handler, delay)
+    contextPID = window.setInterval(handler, delay)
   }
 
   async function getRecommendation<T extends Recommendations[]>(newContext?: any) {
