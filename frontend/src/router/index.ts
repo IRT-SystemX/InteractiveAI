1f4c5a1309f9b6ca451d0cc9bcd036adf7664689
d00r5 <174354444+d00r5@users.noreply.github.com>
2024-02-07 15:00:32 +0100

    chore: rename views and remove lazy loading for cab view

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,8 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import { useAuthStore } from '@/stores/auth'
 import { EntitiesArray, type Entity } from '@/types/entities'
-import HomeView from '@/views/HomeView.vue'
-import LoginView from '@/views/LoginView.vue'
+import CAB from '@/views/CAB.vue'
+import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,7 +12,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: Home,
       meta: {
         auth: true
       }
@@ -19,7 +20,7 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: LoginView,
+      component: Login,
       meta: {
         auth: false
       }
@@ -27,10 +28,7 @@ const router = createRouter({
     {
       path: `/cab/:entity(${EntitiesArray.join('|')})`,
       name: 'cab',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/CABView.vue'),
+      component: CAB,
       meta: {
         auth: true
       }
