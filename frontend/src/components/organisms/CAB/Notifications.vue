80ac0ec6ed85df01e604a6033a73cf740f94d8c0
d00r5 <174354444+d00r5@users.noreply.github.com>
2024-02-09 17:38:41 +0100

    feat: use generic notifications Refs: #228

--- frontend/src/components/organisms/CAB/Notifications.vue
+++ frontend/src/components/organisms/CAB/Notifications.vue
@@ -7,51 +7,52 @@
       @close="modals.splice(modals.indexOf(modal), 1)">
       {{ modal }}
     </Modal>
-    <div class="cab-notifications-main flex flex-col">
-      <section
-        v-for="section of sections"
-        :key="section.name"
-        :style="{ height: `${(section.size / sections.reduce((a, b) => a + b.size, 0)) * 100}%` }">
-        <h1>{{ $t(`cab.notifications.${section.name}`) }}</h1>
-        <div v-if="cardsStore.cards('DA').filter(section.filter).length" class="card-container">
-          <TransitionGroup name="fade">
-            <Notification
-              v-for="card of cardsStore.cards('DA').filter(section.filter)"
-              :key="card.id"
-              :severity="card.severity"
-              @click="eventBus.emit('assistant:selected:DA', card)">
-              <template #title>{{ card.titleTranslated }}</template>
-              <template #severity>
-                <slot name="severity">
-                  {{ format(new Date(card.startDate), 'p') }}
-                  <slot name="icon">
-                    <SVG
-                      src="icons/warning_hex"
-                      :fill="`var(--color-${severityToColor(card.severity)})`"
-                      :width="16"
-                      class="ml-1"></SVG>
-                  </slot>
-                </slot>
-              </template>
-              {{ card.summaryTranslated }}
-              <template #actions>
-                <slot name="actions">
-                  <Button size="small" color="secondary">
-                    <Trash2 :height="12" @click.stop="acknowledge(card)" />
-                  </Button>
-                </slot>
-              </template>
-            </Notification>
-          </TransitionGroup>
-        </div>
-        <div v-else class="card-container-empty">
-          {{ $t('cab.notifications.empty') }}
-        </div>
-      </section>
-    </div>
+    <section
+      v-for="section of sections"
+      :key="section.name"
+      class="cab-notifications flex flex-col"
+      :style="{
+        height: `${(section.weight / sections.reduce((a, b) => a + b.weight, 0)) * 100}%`
+      }">
+      <h1>{{ $t(`cab.notifications.${section.name}`) }}</h1>
+      <div v-if="cards.filter(section.filter).length" class="card-container">
+        <TransitionGroup name="fade">
+          <Notification
+            v-for="card of cards.filter(section.filter)"
+            :key="card.id"
+            :severity="card.severity"
+            @click="selected(card)">
+            <template #title>{{ card.titleTranslated }}</template>
+            <template #severity>
+              <slot name="severity" :card="card">
+                {{ format(new Date(card.startDate), 'p') }}
+              </slot>
+              <slot name="icon" :card="card">
+                <SVG
+                  src="icons/warning_hex"
+                  :fill="`var(--color-${severityToColor(card.severity)})`"
+                  :width="16"
+                  class="ml-1"></SVG>
+              </slot>
+            </template>
+            {{ card.summaryTranslated }}
+            <template #actions>
+              <slot name="actions" :card="card">
+                <Button size="small" color="secondary">
+                  <Trash2 :height="12" @click.stop="acknowledge(card)" />
+                </Button>
+              </slot>
+            </template>
+          </Notification>
+        </TransitionGroup>
+      </div>
+      <div v-else class="card-container-empty">
+        {{ $t('cab.notifications.empty') }}
+      </div>
+    </section>
   </section>
 </template>
-<script setup lang="ts">
+<script setup lang="ts" generic="T extends Entity">
 import { Trash2 } from 'lucide-vue-next'
 import { ref } from 'vue'
 
@@ -64,16 +65,50 @@ import { format } from '@/plugins/date'
 import eventBus from '@/plugins/eventBus'
 import { useCardsStore } from '@/stores/cards'
 import type { Card } from '@/types/cards'
+import type { Entity } from '@/types/entities'
 import { severityToColor } from '@/utils/utils'
 
 const cardsStore = useCardsStore()
 
-withDefaults(
-  defineProps<{ sections?: { name: string; size: number; filter: (card: Card) => boolean }[] }>(),
+const props = withDefaults(
+  defineProps<{
+    sections?: { name: string; weight: number; filter: (card: Card) => boolean }[]
+    entity: T
+  }>(),
   {
-    sections: () => [{ name: 'notification', size: 1, filter: () => true }]
+    sections: () => [{ name: 'main', weight: 1, filter: () => true }]
   }
 )
 
+const cards = cardsStore.cards(props.entity)
+
 const modals = ref<any[]>([])
+
+function selected(card: Card<T>) {
+  // @ts-ignore
+  eventBus.emit(`assistant:selected:${props.entity}`, card)
+}
 </script>
+<style lang="scss">
+.cab-notifications {
+  .card-container {
+    overflow: auto;
+    scrollbar-gutter: stable;
+    height: 100%;
+    display: flex;
+    gap: var(--spacing-1);
+    flex-direction: column;
+    scroll-snap-type: y mandatory;
+  }
+
+  .card-container-empty {
+    display: flex;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    color: var(--color-grey-300);
+    font-weight: 400;
+    text-align: center;
+  }
+}
+</style>
