5a57af4eee735060aba75b1f3bdbb706b3734b58
d00r5 <174354444+d00r5@users.noreply.github.com>
2024-02-09 17:38:41 +0100

    feat: use generic notifications Refs: #228

--- frontend/src/entities/RTE/CAB/Notifications.vue
+++ frontend/src/entities/RTE/CAB/Notifications.vue
@@ -1,95 +1,52 @@
 <template>
-  <section class="cab-panel">
-    <div class="cab-notifications-main flex flex-col">
-      <h1>{{ $t('cab.notifications') }}</h1>
-      <div
-        v-if="
-          cardsStore
-            .cards('RTE')
-            // TODO: better filter
-            .filter((card) => ['ALARM', 'ACTION', 'COMPLIANT', 'ND'].includes(card.severity)).length
-        "
-        class="card-container">
-        <TransitionGroup name="fade">
-          <Notification
-            v-for="card of cardsStore
-              .cards('RTE')
-              .filter((card) => ['ALARM', 'ACTION', 'COMPLIANT', 'ND'].includes(card.severity))"
-            :key="card.id"
-            :severity="card.severity"
-            @click="eventBus.emit('assistant:selected:RTE', card)">
-            <template #title>{{ card.titleTranslated }}</template>
-            <template #severity>
-              Sûreté
-              <Zap
-                v-if="card.severity === 'ALARM'"
-                :fill="`var(--color-${severityToColor(card.severity)})`"
-                :height="16" />
-              <SVG
-                v-else
-                src="icons/toolbox"
-                :fill="`var(--color-${severityToColor(card.severity)})`"
-                :width="16"
-                class="ml-1"></SVG>
-            </template>
-            {{ card.summaryTranslated }}
-            <template #actions>
-              <Button size="small" color="secondary">
-                <ChevronUp :height="16" />
-              </Button>
-              <Button size="small" color="secondary">
-                <ChevronDown :height="16" />
-              </Button>
-              <Button size="small" color="secondary">
-                <MoveRight :height="12" />
-                <User :height="12" />
-              </Button>
-            </template>
-          </Notification>
-        </TransitionGroup>
-      </div>
-      <div v-else class="card-container-empty">
-        {{ $t('cab.notifications.empty') }}
-      </div>
-    </div>
-    <div class="cab-notifications-sub flex flex-col">
-      <h1>{{ $t('cab.notifications.sub') }}</h1>
-      <!--TODO-->
-      <div
-        v-if="
-          cardsStore.cards('RTE').filter((card) => ['INFORMATION'].includes(card.severity)).length
-        "
-        class="card-container">
-        <TransitionGroup name="fade">
-          <Notification
-            v-for="card of cardsStore
-              .cards('RTE')
-              .filter((card) => ['INFORMATION'].includes(card.severity))"
-            :key="card.id"
-            :severity="card.severity"
-            @click="eventBus.emit('assistant:selected:RTE', card)">
-            <template #title>{{ card.titleTranslated }}</template>
-            <template #severity>{{ card.severity }}</template>
-            {{ card.summaryTranslated }}
-            <template #actions><Button size="small" color="secondary">></Button></template>
-          </Notification>
-        </TransitionGroup>
-      </div>
-      <div v-else class="card-container-empty">
-        {{ $t('cab.notifications.empty') }}
-      </div>
-    </div>
-  </section>
+  <Notifications
+    entity="RTE"
+    :sections="[
+      {
+        name: 'main',
+        weight: 2,
+        filter: (card) => filter.includes(card.severity)
+      },
+      {
+        name: 'sub',
+        weight: 1,
+        filter: (card) => !filter.includes(card.severity)
+      }
+    ]">
+    <template #severity>Sûreté</template>
+    <template #icon="{ card }">
+      <Zap
+        v-if="card.severity === 'ALARM'"
+        :fill="`var(--color-${severityToColor(card.severity)})`"
+        :height="16" />
+      <SVG
+        v-else
+        src="icons/toolbox"
+        :fill="`var(--color-${severityToColor(card.severity)})`"
+        :width="16"
+        class="ml-1"></SVG>
+    </template>
+    <template #actions>
+      <Button size="small" color="secondary">
+        <ChevronUp :height="16" />
+      </Button>
+      <Button size="small" color="secondary">
+        <ChevronDown :height="16" />
+      </Button>
+      <Button size="small" color="secondary">
+        <MoveRight :height="12" />
+        <User :height="12" />
+      </Button>
+    </template>
+  </Notifications>
 </template>
 <script setup lang="ts">
 import { ChevronDown, ChevronUp, MoveRight, User, Zap } from 'lucide-vue-next'
 
 import Button from '@/components/atoms/Button.vue'
 import SVG from '@/components/atoms/SVG.vue'
-import Notification from '@/components/molecules/Notification.vue'
-import eventBus from '@/plugins/eventBus'
-import { useCardsStore } from '@/stores/cards'
+import Notifications from '@/components/organisms/CAB/Notifications.vue'
 import { severityToColor } from '@/utils/utils'
 
-const cardsStore = useCardsStore()
+const filter = ['ALARM', 'ACTION', 'COMPLIANT', 'ND']
 </script>
