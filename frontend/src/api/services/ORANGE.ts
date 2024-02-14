9c7d3eb732d00c1d772ee41f3711e82c375a5dea
Thomas ENGELS <thomas.engels@ext.irt-systemx.fr>
2024-02-14 16:03:03 +0100

    feat(ORANGE): [TS-Orange-V4-300] Frontend – Call API on click on feedback thumbs

--- frontend/src/api/services/ORANGE.ts
+++ frontend/src/api/services/ORANGE.ts
@@ -1,8 +1,18 @@
 import type { CorrelationResponse } from '@/entities/ORANGE/types'
 import http from '@/plugins/http'
+import type { Card } from '@/types/cards'
 
 export function getCorrelations(params: { size: number; app_id?: string; kpi_name?: string }) {
   return http.get<CorrelationResponse>('cab_correlation/api/v1/correlation', {
     params
   })
+}
+export function correlation_feedback(card: Card, feedback: boolean) {
+  const data = {
+    card: card,
+    feedback: feedback,
+    feedback_date: Date.now()
+  }
+  console.log(data)
+  return http.post('cab_correlation/api/v1/correlation/feedback', data)
 }
