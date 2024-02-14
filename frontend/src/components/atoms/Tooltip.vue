98198858ad9532d8ef191a53e5b42041eef16828
d00r5 <174354444+d00r5@users.noreply.github.com>
2024-02-14 16:06:38 +0100

    feat: confirmation modale for deletion Refs: #232

---
+++ frontend/src/components/atoms/Tooltip.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="cab-tooltip">
+    <slot />
+    <div class="cab-tooltip-container"><slot name="tooltip"></slot></div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style lang="scss">
+.cab-tooltip {
+  display: inline-block;
+  border-bottom: 1px dotted black; /* If you want dots under the hoverable text */
+  &:hover &-container {
+    visibility: visible;
+  }
+  &-container {
+    visibility: hidden;
+    width: 120px;
+    background-color: black;
+    color: #fff;
+    text-align: center;
+    padding: 5px 0;
+    border-radius: 6px;
+    transform: translateX(-50%);
+
+    /* Position the tooltip text - see examples below! */
+    position: absolute;
+    z-index: 1;
+    &::after {
+      content: ' ';
+      position: absolute;
+      bottom: 100%; /* At the top of the tooltip */
+      left: 50%;
+      margin-left: -5px;
+      border-width: 5px;
+      border-style: solid;
+      border-color: transparent transparent black transparent;
+    }
+  }
+}
+</style>
