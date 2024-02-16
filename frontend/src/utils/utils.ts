84516513fd59525699804498bc4d8e745d82dfb7
d00r5 <174354444+d00r5@users.noreply.github.com>
2024-02-16 15:45:49 +0100

    fix: randomUUID if secure, fallback else

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -117,7 +117,9 @@ export function addOrUpdate<T>(
 }
 
 export function uuid() {
-  return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
-    (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16)
-  ) as UUID
+  return window.isSecureContext
+    ? crypto.randomUUID()
+    : ('10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
+        (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16)
+      ) as UUID)
 }
