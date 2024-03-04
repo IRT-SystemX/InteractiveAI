1163b0999d5d05d58aa511d8cfbba07f1a6b4d1c
d00r5 <174354444+d00r5@users.noreply.github.com>
2024-03-04 16:13:24 +0100

    chore: put entity specific APIs in entity's folder Refs: #220

--- frontend/src/api/services/SNCF.ts
+++
@@ -1,6 +0,0 @@
-import http from '@/plugins/http'
-import type { Action } from '@/types/entities'
-
-export function applyRecommendation(data: Action<'SNCF'>) {
-  return http.post<{ message: string }>(import.meta.env.VITE_SNCF_SIMU + '/transport_plan', data)
-}
