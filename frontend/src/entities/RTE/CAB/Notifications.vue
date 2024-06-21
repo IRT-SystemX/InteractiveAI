6530df8f1bfa6c9e7cefbf154e350bfe0e0c0a07
d00r5 <174354444+d00r5@users.noreply.github.com>
2024-06-21 10:39:17 +0200

    fix(RTE): remove up and down vote Refs: #336

--- frontend/src/entities/RTE/CAB/Notifications.vue
+++ frontend/src/entities/RTE/CAB/Notifications.vue
@@ -28,18 +28,6 @@
         class="ml-1"></SVG>
     </template>
     <template #actions="{ card, deletion, hasBeenAcknowledged }">
-      <Tooltip>
-        <template #tooltip>{{ $t('card.actions.upvote.tooltip') }}</template>
-        <Button size="small" color="secondary" @click.stop="vote(card, true, $event)">
-          <ChevronUp :height="16" />
-        </Button>
-      </Tooltip>
-      <Tooltip>
-        <template #tooltip>{{ $t('card.actions.downvote.tooltip') }}</template>
-        <Button size="small" color="secondary" @click.stop="vote(card, false, $event)">
-          <ChevronDown :height="16" />
-        </Button>
-      </Tooltip>
       <Tooltip v-if="!hasBeenAcknowledged">
         <template #tooltip>{{ $t('card.actions.delete.tooltip') }}</template>
         <Button size="small" color="secondary" @click.stop="deletion(card)">
@@ -50,34 +38,14 @@
   </Notifications>
 </template>
 <script setup lang="ts">
-import { ChevronDown, ChevronUp, Inbox, Zap } from 'lucide-vue-next'
-import { ref } from 'vue'
+import { Inbox, Zap } from 'lucide-vue-next'
 
-import { removeEvent } from '@/api/cards'
 import Button from '@/components/atoms/Button.vue'
 import SVG from '@/components/atoms/SVG.vue'
 import Tooltip from '@/components/atoms/Tooltip.vue'
 import Notifications from '@/components/organisms/CAB/Notifications.vue'
-import { useAppStore } from '@/stores/app'
-import type { Card, Criticality } from '@/types/cards'
+import type { Criticality } from '@/types/cards'
 import { criticalityToColor } from '@/utils/utils'
 
 const FILTER: Criticality[] = ['HIGH', 'MEDIUM', 'LOW', 'ND']
-
-const appStore = useAppStore()
-
-const voted = ref<boolean>()
-
-async function vote(card: Card<'RTE'>, up: boolean, event: any) {
-  if (!up) {
-    if (appStore._card?.id === card.id) appStore._card = undefined
-    await removeEvent(card.processInstanceId)
-  }
-  voted.value = up
-  event.target.classList.add('success')
-  setTimeout(() => {
-    voted.value = undefined
-    event.target.classList.remove('success')
-  }, 2000)
-}
 </script>
