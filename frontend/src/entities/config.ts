6403d3439c1bddef53fae656d4953115793a0156
Abderrahman AIT SAID <aitsaidabderrahman@gmail.com>
2025-02-12 08:01:54 +0000

    update users and types on entities

--- frontend/src/entities/config.ts
+++ frontend/src/entities/config.ts
@@ -1,15 +1,18 @@
 // Import your theme here
 import './PowerGrid/assets/theme.scss'
 import './Railway/assets/theme.scss'
+import './ATM/assets/theme.scss'
 
 // Import your types here
 import type { PowerGrid } from './PowerGrid/types'
 import type { Railway } from './Railway/types'
+import type { ATM } from './ATM/types'
 // Add your entity and config here
 // darkMode: use dark mode
 export const ENTITIES_CONFIG = {
   PowerGrid: { darkMode: false },
-  Railway: {darkMode: false}
+  Railway: {darkMode: false},
+  ATM: {darkMode: false}
 } as const
 Object.freeze(ENTITIES_CONFIG)
 
@@ -17,4 +20,5 @@ Object.freeze(ENTITIES_CONFIG)
 export type EntitiesTypes = {
   PowerGrid: PowerGrid
   Railway: Railway
+  ATM: ATM
 }
